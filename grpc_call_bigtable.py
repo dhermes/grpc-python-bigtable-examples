4fdae770df6ec71295e7365ad79539e2bdd774c1
Danny Hermes <daniel.j.hermes@gmail.com>
2015-07-15 21:16:37 -0700

    Updating for ListTables.
    BigTable Cluster Admin requests fail for a service account.

--- grpc_call_bigtable.py
+++ grpc_call_bigtable.py
@@ -1,22 +1,25 @@
 import os
-import traceback
 
 from oauth2client.client import _get_application_default_credential_from_file
 
-from gcloud_bigtable._generated import bigtable_cluster_service_messages_pb2
-from gcloud_bigtable._generated import bigtable_cluster_service_pb2
+from gcloud_bigtable._generated import bigtable_table_service_messages_pb2
+from gcloud_bigtable._generated import bigtable_table_service_pb2
 
 
 PROJECT_ID = '1234PROJECTID'
 JSON_PATH = 'path/to/credentials_file.json'
+ZONE = 'us-central1-b'
+CLUSTER = 'foo-bar-baz-1262357y324823'
 
-STUB_CLASS = (bigtable_cluster_service_pb2.
-              early_adopter_create_BigtableClusterService_stub)
-TABLE_SCOPE = 'https://www.googleapis.com/auth/bigtable.admin.table'
-CLUSTER_SCOPE = 'https://www.googleapis.com/auth/bigtable.admin.cluster'
+STUB_CLASS = (bigtable_table_service_pb2.
+              early_adopter_create_BigtableTableService_stub)
+BASE_SCOPE = 'https://www.googleapis.com/auth/cloud-bigtable.data'
+TABLE_SCOPE = 'https://www.googleapis.com/auth/cloud-bigtable.admin'
+CLUSTER_SCOPE = 'https://www.googleapis.com/auth/cloud-bigtable.admin'
 SSL_CERT_FILE = '/etc/ssl/certs/ca-certificates.crt'
 CREDENTIALS = _get_application_default_credential_from_file(JSON_PATH)
-SCOPED_CREDENTIALS = CREDENTIALS.create_scoped([TABLE_SCOPE, CLUSTER_SCOPE])
+SCOPED_CREDENTIALS = CREDENTIALS.create_scoped([BASE_SCOPE, TABLE_SCOPE,
+                                                CLUSTER_SCOPE])
 ACCESS_TOKEN = SCOPED_CREDENTIALS.get_access_token().access_token
 del CREDENTIALS
 del SCOPED_CREDENTIALS
@@ -39,19 +42,20 @@ stub = STUB_CLASS(HOST, PORT,
                   private_key=None,
                   certificate_chain=None,
                   server_host_override=None)
-request_pb = bigtable_cluster_service_messages_pb2.ListZonesRequest(
-    name='projects/' + PROJECT_ID)
-with stub:
-    response = stub.ListZones.async(request_pb, TIMEOUT_SECONDS)
 
-print('response.running():')
-print(response.running())
-print('response.done():')
-print(response.done())
-print('response.cancelled():')
-print(response.cancelled())
-print('response.exception():')
-print(response.exception())
-tb = response.traceback()
-traceback.print_tb(tb)
-response.result()
+table_name = 'projects/%s/zones/%s/clusters/%s' % (
+    PROJECT_ID, ZONE, CLUSTER)
+request_pb = bigtable_table_service_messages_pb2.ListTablesRequest(
+    name=table_name)
+with stub:
+    response = stub.ListTables.async(request_pb, TIMEOUT_SECONDS)
+    print('response.running():')
+    print(response.running())
+    print('response.done():')
+    print(response.done())
+    print('response.cancelled():')
+    print(response.cancelled())
+    print('response.exception():')
+    print(repr(response.exception()))
+    print('response.result():')
+    print(response.result())
