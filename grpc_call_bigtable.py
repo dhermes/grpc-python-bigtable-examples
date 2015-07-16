6ba98ff70bb37c2afaebf286e7717fbb9d0262fd
Danny Hermes <daniel.j.hermes@gmail.com>
2015-07-15 21:47:27 -0700

    Moving auth to util.

--- grpc_call_bigtable.py
+++ grpc_call_bigtable.py
@@ -1,49 +1,32 @@
 import json
 import os
 
-from oauth2client.client import _get_application_default_credential_from_file
-
 from gcloud_bigtable._generated import bigtable_table_service_messages_pb2
 from gcloud_bigtable._generated import bigtable_table_service_pb2
 
-from config import PROJECT_ID
-from config import KEYFILE_PATH
-from config import ZONE
 from config import CLUSTER
+from config import PROJECT_ID
+from config import TIMEOUT_SECONDS
+from config import ZONE
+from util import PORT
+from util import TABLE_ADMIN_HOST
+from util import get_certs
+from util import get_token
 from util import protobuf_to_dict
 
 
 STUB_CLASS = (bigtable_table_service_pb2.
               early_adopter_create_BigtableTableService_stub)
-BASE_SCOPE = 'https://www.googleapis.com/auth/cloud-bigtable.data'
-TABLE_SCOPE = 'https://www.googleapis.com/auth/cloud-bigtable.admin'
-CLUSTER_SCOPE = 'https://www.googleapis.com/auth/cloud-bigtable.admin'
-SSL_CERT_FILE = '/etc/ssl/certs/ca-certificates.crt'
-CREDENTIALS = _get_application_default_credential_from_file(KEYFILE_PATH)
-SCOPED_CREDENTIALS = CREDENTIALS.create_scoped([BASE_SCOPE, TABLE_SCOPE,
-                                                CLUSTER_SCOPE])
-ACCESS_TOKEN = SCOPED_CREDENTIALS.get_access_token().access_token
-del CREDENTIALS
-del SCOPED_CREDENTIALS
-AUTH_HEADER = 'Bearer ' + ACCESS_TOKEN
-TIMEOUT_SECONDS = 10
-HOST = 'bigtabletableadmin.googleapis.com'
-PORT = 443
-with open(SSL_CERT_FILE, mode='rb') as file_obj:
-    ROOT_CERTIFICATES = file_obj.read()
 
 
 def custom_metadata_transformer(ignored_val):
-    return [('Authorization', AUTH_HEADER)]
+    return [('Authorization', 'Bearer ' + get_token())]
 
 
-stub = STUB_CLASS(HOST, PORT,
+stub = STUB_CLASS(TABLE_ADMIN_HOST, PORT,
                   metadata_transformer=custom_metadata_transformer,
                   secure=True,
-                  root_certificates=ROOT_CERTIFICATES,
-                  private_key=None,
-                  certificate_chain=None,
-                  server_host_override=None)
+                  root_certificates=get_certs())
 
 table_name = 'projects/%s/zones/%s/clusters/%s' % (
     PROJECT_ID, ZONE, CLUSTER)
--- util.py
+++ util.py
@@ -1,4 +1,21 @@
 from google.protobuf import internal
+from oauth2client.client import _get_application_default_credential_from_file
+
+from config import KEYFILE_PATH
+
+
+BASE_SCOPE = 'https://www.googleapis.com/auth/cloud-bigtable.data'
+TABLE_SCOPE = 'https://www.googleapis.com/auth/cloud-bigtable.admin'
+CLUSTER_SCOPE = 'https://www.googleapis.com/auth/cloud-bigtable.admin'
+SSL_CERT_FILE = '/etc/ssl/certs/ca-certificates.crt'
+PORT = 443
+TABLE_ADMIN_HOST = 'bigtabletableadmin.googleapis.com'
+
+
+class AuthInfo(object):
+
+    ACCESS_TOKEN = None
+    ROOT_CERTIFICATES = None
 
 
 def protobuf_to_dict(pb_value):
@@ -11,3 +28,37 @@ def protobuf_to_dict(pb_value):
             value = list(value)
         result[name] = value
     return result
+
+
+def _set_token():
+    credentials = _get_application_default_credential_from_file(
+        KEYFILE_PATH)
+    scoped_credentials = credentials.create_scoped(
+        [BASE_SCOPE, TABLE_SCOPE, CLUSTER_SCOPE])
+    access_token = scoped_credentials.get_access_token().access_token
+    AuthInfo.ACCESS_TOKEN = access_token
+
+
+def set_token(reset=False):
+    if AuthInfo.ACCESS_TOKEN is None or reset:
+        _set_token()
+
+
+def get_token():
+    set_token(reset=False)
+    return AuthInfo.ACCESS_TOKEN
+
+
+def _set_certs():
+    with open(SSL_CERT_FILE, mode='rb') as file_obj:
+        AuthInfo.ROOT_CERTIFICATES = file_obj.read()
+
+
+def set_certs(reset=False):
+    if AuthInfo.ROOT_CERTIFICATES is None or reset:
+        _set_certs()
+
+
+def get_certs():
+    set_certs(reset=False)
+    return AuthInfo.ROOT_CERTIFICATES
